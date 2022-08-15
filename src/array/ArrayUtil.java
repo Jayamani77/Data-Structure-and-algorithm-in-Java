94e770994fe8f0309e1435d5f6898f363ea79c2d
Jayamani77 <svjayamanikandan77@gmail.com>
2022-08-15 15:08:29 +0530

    Code Added

--- src/array/ArrayUtil.java
+++ src/array/ArrayUtil.java
@@ -106,6 +106,28 @@ public class ArrayUtil {
         }
         return true;
     }
+    public static int[] sortedSquares(int[] arr){
+        int n=arr.length;
+        int i=0;int j=n-1;
+        int[] result=new int[n];
+        
+        for(int k=n-1;k>=0;k--){
+            if(Math.abs(arr[i])>Math.abs(arr[j])){
+                result[k]=arr[i]*arr[i];
+                i++;
+            }
+            else{
+                result[k]=arr[j]*arr[j];
+                j--;
+            }
+        }
+        return result;
+    }
+    public void arrayDemo(){
+        int[] arr={-4,-1,0,3,10};
+        int[] result=sortedSquares(arr);
+        printArray(result);
+    }
     public static void main(String[] args){
         ArrayUtil au=new ArrayUtil();
         int[] arr={0,2,4,0,10,0,5};
@@ -127,5 +149,7 @@ public class ArrayUtil {
         else{
             System.out.println("The String is not Palindrome???");
         }
+        
+        au.arrayDemo();
     } 
 }
